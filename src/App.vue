396f652c9428fac072c6c9fe0ff4b7c82f0ae738
Lyn Ho <lyn1107130806@gmail.com>
2019-03-26 21:01:10 +0800

    Init a vue app.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div#app {{ message }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'test test',
+    };
+  },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      {
+        test: /\.pug$/,
+        use: 'pug-plain-loader'
+      },
+      {
+        test: /\.(c|le)ss$/,
+        use: ['vue-style-loader', 'css-loader', 'less-loader']
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.vue', '.css', '.less']
+  },
+
+  devServer: {
+    open: true,
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
